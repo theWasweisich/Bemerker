3e2dbda4841172b7dbd3d22f0b29e7c797ad6d24
theWasweisich <lang.benny.d@gmail.com>
2021-07-09 15:53:03 +0200

    Schönheit + mehr Funktionen
    hab schon wieder vergessen was, aber man kann jetzt den Titel der Bemerkungen einstellen, sowie die Zeit

--- Bemerker 2.1.py
+++ Bemerker 2.1.py
@@ -1,38 +1,37 @@
 import time
 import win10toast
+from win10toast import ToastNotifier
 import pyautogui
 import random
-
-
-from win10toast import ToastNotifier
 
 toaster = ToastNotifier()
 liste = []
 fertige_liste = []
 einstellungen = []
+title = ("")
 datei = open('bemerkungen.txt','r')
 
 for zeile in datei:
     liste.append(zeile)
-
 datei.close()
+
+for element in liste:
+    fertige_liste.append(element.strip())
 
 with open('Einstellungen.txt', "r") as einst:
     for zeile in einst:
         einstellungen.append(zeile)
     einst.close()
 
+title = str(einstellungen[6].strip())
 
-for element in liste:
-    fertige_liste.append(element.strip())
-
-print("Achtung! Fenster nicht schließen, sondern minimieren. Ansonsten wird das Programm gestoppt.")
-
+print("\nAchtung! Fenster nicht schließen, sondern minimieren. Ansonsten wird das Programm gestoppt.\n")
+zeit = str(einstellungen[3].strip())
+print('Einstellungen: \nZeit: ' + zeit + '\nTitel: ' + title)
 time.sleep(3)
 
 print('\n\nAber nun: viel Spaß!')
 
-print(fertige_liste)
 
 time.sleep(0.5)
 
@@ -41,10 +40,10 @@ bemerkung = ""
 while True:
     bemerkung = random.choice(fertige_liste)
     print('\n\n>    sending Notification: "' + bemerkung + '" ...')
-    toaster.show_toast("Bemerkung", bemerkung, icon_path="icon.ico")
+    toaster.show_toast(title, bemerkung, icon_path="icon.ico")
     bemerkung = ""
     zeit = int(einstellungen[3])
-    print('>    starting timer...')
+    print('>    ' + str(zeit) + ' seconds till next notificatoion...')
     while zeit != 0:
         zeit = zeit - 1
         time.sleep(1)
