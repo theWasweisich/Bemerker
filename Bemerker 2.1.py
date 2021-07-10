71e7012c64991e2d81746718f069de842c025741
theWasweisich <lang.benny.d@gmail.com>
2021-07-10 09:40:01 +0200

    Update Bemerker 2.1.py
    Das eigendliche Skript

--- Bemerker 2.1.py
+++ Bemerker 2.1.py
@@ -1,50 +1,51 @@
-import time
-import win10toast
-import pyautogui
-import random
+import time #Für den Countdown
+import win10toast #Das Modul für Windows Toasts
+from win10toast import ToastNotifier #Die Toasts von dem Modul für Windows Toasts
+import random #Für die randomisierung
 
-
-from win10toast import ToastNotifier
-
+# Vorbereitungen
 toaster = ToastNotifier()
 liste = []
 fertige_liste = []
 einstellungen = []
+title = ("")
 datei = open('bemerkungen.txt','r')
 
+# Auslesen und conventieren der Bemerkungen
 for zeile in datei:
     liste.append(zeile)
-
 datei.close()
-
-with open('Einstellungen.txt', "r") as einst:
-    for zeile in einst:
-        einstellungen.append(zeile)
-    einst.close()
-
 
 for element in liste:
     fertige_liste.append(element.strip())
 
-print("Achtung! Fenster nicht schließen, sondern minimieren. Ansonsten wird das Programm gestoppt.")
+# Einlesen und Auswerten der Einstellungen
+with open('Einstellungen.txt', "r") as einst:
+    for zeile in einst:
+        einstellungen.append(zeile)
+    einst.close()
+title = str(einstellungen[6].strip())
 
+print("\nAchtung! Fenster nicht schließen, sondern minimieren. Ansonsten wird das Programm gestoppt.\n")
+zeit = str(einstellungen[3].strip())
+print('Einstellungen: \nZeit: ' + zeit + '\nTitel: ' + title)
 time.sleep(3)
 
 print('\n\nAber nun: viel Spaß!')
 
-print(fertige_liste)
 
 time.sleep(0.5)
 
 bemerkung = ""
 
+# Eigendliche Schleife
 while True:
     bemerkung = random.choice(fertige_liste)
     print('\n\n>    sending Notification: "' + bemerkung + '" ...')
-    toaster.show_toast("Bemerkung", bemerkung, icon_path="icon.ico")
+    toaster.show_toast(title, bemerkung, icon_path="icon.ico")
     bemerkung = ""
     zeit = int(einstellungen[3])
-    print('>    starting timer...')
+    print('>    ' + str(zeit) + ' seconds till next notificatoion...')
     while zeit != 0:
         zeit = zeit - 1
         time.sleep(1)
